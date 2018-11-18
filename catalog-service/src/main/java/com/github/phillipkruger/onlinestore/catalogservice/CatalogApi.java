bfcb369608e908be65a8884215aec2e5fc3f40ad
Phillip Kruger <phillip.kruger@gmail.com>
2018-11-18 11:39:21 +0200

    Added basic ping

--- catalog-service/src/main/java/com/github/phillipkruger/onlinestore/catalogservice/CatalogApi.java
+++ catalog-service/src/main/java/com/github/phillipkruger/onlinestore/catalogservice/CatalogApi.java
@@ -1,5 +1,7 @@
 package com.github.phillipkruger.onlinestore.catalogservice;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,9 +10,11 @@ import lombok.extern.java.Log;
 @Path("/catalog")
 @RequestScoped
 @Log
+@Api(value = "Catalog service")
 public class CatalogApi {
     
     @GET
+    @ApiOperation(value = "Ping", notes = "Testing availability")
     public String ping(){
         return "pong";
     }
